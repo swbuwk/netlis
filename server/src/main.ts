1419fe57219d9da767a0896e2d0c5569554aca75
swbuwk <khhkhhkkh@gmail.com>
2022-09-17 22:36:21 +0300

    added exception filter and cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from './track/validation.pipe';
+import { ValidationPipe } from './validation.pipe';
+import * as cookieParser from 'cookie-parser';
+import { ServerExceptionFilter } from './server-exception.filter';
 
 async function bootstrap() {
   try {
     const PORT = process.env.PORT || 5000;
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, {cors: {origin: "http://localhost:3000", credentials: true }});
+    app.use(cookieParser());
     app.useGlobalPipes(new ValidationPipe())
+    app.useGlobalFilters(new ServerExceptionFilter())
     await app.listen(PORT, () => {
         console.log(`Server started on port ${PORT}`)
     });
